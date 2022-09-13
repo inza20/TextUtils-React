c2c3f18ca4fcd12c1ff034bddde4cafb486be00a
Inshirah Zainab <inshirahzainab@gmail.com>
2022-09-13 23:13:43 +0530

    Updates

---
+++ precache-manifest.7969669cf2b28fb06318a36611707af2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14dcf1ee297f5eb15357",
+    "url": "/TextUtils-React/static/css/main.57f3eae2.chunk.css"
+  },
+  {
+    "revision": "14dcf1ee297f5eb15357",
+    "url": "/TextUtils-React/static/js/main.14dcf1ee.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "ab085fe389889eba7ca6f84a85d78990",
+    "url": "/TextUtils-React/index.html"
+  }
+];
